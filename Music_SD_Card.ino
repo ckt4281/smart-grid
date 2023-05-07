7aaa5b4edc3504e5c4537059247fea5a12c99473
ckt4281 <88560530+ckt4281@users.noreply.github.com>
2023-05-06 19:14:41 -0700

    Add files via upload

---
+++ Music_SD_Card.ino
@@ -0,0 +1,28 @@
+#include "SD.h"
+#define SD_ChipSelectPin 10
+#include "TMRpcm.h"
+#include "SPI.h"
+
+TMRpcm tmrpcm;
+
+void setup()
+{
+tmrpcm.speakerPin=9;
+Serial.begin(9600);
+
+}
+
+void loop() {
+
+if(!SD.begin(SD_ChipSelectPin))
+{
+  Serial.println("SD fail");
+  return;
+}
+
+
+
+tmrpcm.setVolume(5);
+tmrpcm.play("DS.wav");
+
+}
